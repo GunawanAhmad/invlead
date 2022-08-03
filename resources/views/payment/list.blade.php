bb9ef6e96e05e30e2db2aa0d7955edf840444ff0
MaswanPratama <maswanpratama30@gmail.com>
2022-08-03 14:55:58 +0700

    add absensi

--- resources/views/payment/list.blade.php
+++ resources/views/payment/list.blade.php
@@ -1,24 +1,143 @@
 
   
 @include('base.start', ['path' => 'pembayaran', 'title' => 'Pembayaran Tagihan', 'breadcrumbs' => ['Pembayaran']])
-  <div class="row">      
-    @foreach($merchantProducts as $merchantProduct)  
-      <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-        <div class="card h-100">
-          <div class="card-body p-3">
-            <div class="row">
-              <div class="numbers">
-                <a href="{{ url('pembayaran', $merchantProduct->id) }}">
-                    <h5 class="font-weight-bolder">
-                        {{ $merchantProduct->merchant->name }}: {{ $merchantProduct->name }}
-                    </h5>
-                </a>
-              </div>
+<div class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header card-header-primary">
+            <h4 class="card-title ">Absensi</h4>
+            <p class="card-category"> Absensi Peserta Magang Inovindo Digital Media</p>
+          </div>
+          <div class="card-body">
+            <div class="table-responsive">
+              <table class="table">
+                <thead class=" text-primary">
+                  <th>
+                    No
+                  </th>
+                  <th>
+                    Nama Peserta
+                  </th>
+                  <th>
+                    Hadir
+                  </th>
+                  <th>
+                    izin
+                  </th>
+                  <th>
+                    alfa
+                  </th>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      1
+                    </td>
+                    <td>
+                      Dakota Rice
+                    </td>
+                    <td>
+                      Niger
+                    </td>
+                    <td>
+                      Oud-Turnhout
+                    </td>
+                    <td class="text-primary">
+                      $36,738
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      2
+                    </td>
+                    <td>
+                      Minerva Hooper
+                    </td>
+                    <td>
+                      Curaçao
+                    </td>
+                    <td>
+                      Sinaai-Waas
+                    </td>
+                    <td class="text-primary">
+                      $23,789
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      3
+                    </td>
+                    <td>
+                      Sage Rodriguez
+                    </td>
+                    <td>
+                      Netherlands
+                    </td>
+                    <td>
+                      Baileux
+                    </td>
+                    <td class="text-primary">
+                      $56,142
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      4
+                    </td>
+                    <td>
+                      Philip Chaney
+                    </td>
+                    <td>
+                      Korea, South
+                    </td>
+                    <td>
+                      Overland Park
+                    </td>
+                    <td class="text-primary">
+                      $38,735
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      5
+                    </td>
+                    <td>
+                      Doris Greene
+                    </td>
+                    <td>
+                      Malawi
+                    </td>
+                    <td>
+                      Feldkirchen in Kärnten
+                    </td>
+                    <td class="text-primary">
+                      $63,542
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      6
+                    </td>
+                    <td>
+                      Mason Porter
+                    </td>
+                    <td>
+                      Chile
+                    </td>
+                    <td>
+                      Gloucester
+                    </td>
+                    <td class="text-primary">
+                      $78,615
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
       </div>
-    @endforeach   
-  </div>
 
 @include('base.end')
